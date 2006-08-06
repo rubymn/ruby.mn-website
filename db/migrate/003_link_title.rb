64b1b171d671eac1f3d1c8b3bed16e7d8afd8598
mml@ld50.local <none@none>
2006-08-05 21:56:27 -0500

    removed defunct migration

--- db/migrate/003_link_title.rb
+++
@@ -1,9 +0,0 @@
-class LinkTitle < ActiveRecord::Migration
-  def self.up
-      add_column :links, :user_id, :integer
-      add_column :links, :title, :string, :maxlength=>150
-  end
-
-  def self.down
-  end
-end
