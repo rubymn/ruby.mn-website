0848c693eef8bf834333c3c49071feb86e8e78e1
m@loonsoft.com <none@none>
2007-03-22 09:20:21 -0500

    limit list of events

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-      @events = Event.find :all, :order=>"scheduled_time desc"
+    @events = Event.find :all, :order=>"scheduled_time desc", :limit=>5
   end
 end
