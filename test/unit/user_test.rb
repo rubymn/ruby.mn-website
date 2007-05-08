c4cbfaa8656e1b85fb567eab3834e7df63a29395
m@loonsoft.com <none@none>
2007-05-08 15:34:50 -0500

    remove books, fixed tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,15 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
-  fixtures :users, :books
+  fixtures :users
 
 
-  def test_books
-    assert_not_nil users(:bob).books
-    assert_equal 2, users(:bob).books.size
-    assert users(:bob).books.include?(books(:first))
-    assert users(:bob).books.include?(books(:second))
-  end
   def test_relation
     u = users(:bob)
     assert_not_nil u
