6b34d4a800261037f344a92d084844f6fe9ab753
m@loonsoft.com <none@none>
2007-07-25 15:23:36 -0500

    removed show test

--- test/functional/archive_controller_test.rb
+++ test/functional/archive_controller_test.rb
@@ -21,12 +21,4 @@ class ArchiveControllerTest < Test::Unit::TestCase
     assert_template 'message'
   end
 
-  def test_show
-    login_as(:bob)
-    get :show, :id=>list_mails(:list_mails_3285).id
-    assert_response :success
-    assert assigns(:message)
-    assert_equal list_mails(:list_mails_3285), assigns(:message)
-    assert_template 'show'
-  end
 end
