aad7b96855af9394d092432978d7986f0e1f50c8
m@loonsoft.com <none@none>
2007-07-25 21:03:02 -0500

    capistrano 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,25 +10,13 @@ role :db,  "looneys.net", :primary=>true
 set :deploy_to, "/home/mml/tcrbb" # defaults to "/u/apps/#{application}"
 set :scm, :mercurial               # defaults to :subversion
 
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
-task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { delete "/tmp/dump.sql" }
 
-  run "mysqldump -u root > /tmp/tcrbb.sql"
-end
-
+desc "start up the cluster"
 task :spinner, :roles=>:app do
   run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::start"
 end
 
+desc "restart the cluster"
 task :restart, :roles=>:app do
-  transaction do
-    run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::stop"
-    run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::start"
-  end
+    run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::restart"
 end
