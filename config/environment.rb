ad376d3692ac9c6f593b892d3ad32e54f1efc8ef
mml@mail.loonsoft.com <none@none>
2007-11-14 14:51:06 -0600

    update to1.2.5

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
-RAILS_GEM_VERSION='1.2.4'
+RAILS_GEM_VERSION='1.2.5'
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
