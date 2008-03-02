9a43b7d636b9ebf91d4f1b038647f5b38da46c46
m@loonsoft.com <none@none>
2008-03-02 07:23:51 -0600

    m

--- db/migrate/021_openings_cat.rb
+++
@@ -1,9 +0,0 @@
-class OpeningsCat < ActiveRecord::Migration
-  def self.up
-    add_column :openings, :created_at, :datetime
-  end
-
-  def self.down
-    remove_column :openings, :created_at, :datetime
-  end
-end
