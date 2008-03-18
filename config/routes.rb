e5bbbfb6dc89792d660b576fbd4bc1b0631015a4
mml@mail.loonsoft.com <none@none>
2008-03-18 10:02:02 -0500

    fixed busted url in confirmation mail

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
   map.connect '', :controller => "welcome", :action=>'index'
 
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
-
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
