711ab9d3224fb8ae04b1f029df22855213f50bcf
m@loonsoft.com <none@none>
2008-12-18 10:37:56 -0600

    lets quote strings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,4 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-TCRBB_LIST_ADDRESS=ruby.mn@ruby.mn
+TCRBB_LIST_ADDRESS='ruby.mn@ruby.mn'
