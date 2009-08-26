b3999716fe8160c50bd4835e0d8af95198686c45
mml <none@none>
2009-08-26 16:20:14 -0500

    removed another set of mail config, updated list address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,4 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # rake db:test:data:load
 config.action_mailer.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = {
-  :address => "mail.loonsoft.com",
-  :port => 25,
-  :domain => "ruby.mn",
-  :enable_starttls_auto => false
-}
-config.action_mailer.delivery_method = :smtp
-TCRBB_LIST_ADDRESS='ruby.mn@ruby.mn'
+TCRBB_LIST_ADDRESS='ruby_mn@googlegroups.com'
