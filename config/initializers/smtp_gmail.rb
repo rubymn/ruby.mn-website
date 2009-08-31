85a142eb1b6d98f1f24d2a1901b8d355658da6a8
mml <none@none>
2009-08-31 10:28:30 -0500

    hmm

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -1,14 +1,12 @@
 # This file is automatically copied into RAILS_ROOT/initializers
 
-require "smtp_tls"
-
-config_file = "#{RAILS_ROOT}/config/smtp_gmail.yml"
-raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
-
-config_options = YAML.load_file(config_file) 
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
+config.action_mailer.smtp_settings = {
+  :address        => "smtp.gmail.com",
+  :port           => 587,
+  :domain         => "ruby.mn.bot@gmail.com",
   :authentication => :plain,
-  :enable_starttls_auto => true
-}.merge(config_options) # Configuration options override default options
+  :user_name      => "ruby.mn.bot@gmail.com",
+  :password       => "thisisanexample" 
+}
+
+
