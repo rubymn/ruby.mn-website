d309a7bd7eb90e490a81c73f39e9b8ee79cf422b
mml <none@none>
2010-01-26 12:00:55 -0600

    update to rails 2.3.5, added paperclip support

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "ruby-recaptcha"
   config.gem "haml"
   config.gem "BlueCloth", :lib=>'bluecloth'
+  config.gem "paperclip"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
