937931e49c9890a22fda748bbef3b5b8987d9de6
Andy Atkinson <andy@webandy.com>
2010-07-12 23:33:21 -0400

    making some style fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(function() {
-  $('#map').hide();
-  $('#hide_map').live('click', function(){
-    $('#map').hide(); 
-  })
-  $('#view_map').click(function(e) {
-    e.preventDefault();
-    var link = $(this);
-    $.get(link.attr('href'), function(data) {
-      data += "<br/><a href='#' id='hide_map'>Hide Map</a>";
-      $('#map').html(data).fadeIn().show();
-    });
-  });
-});
+
 
 // shamelessly stolen from @webandy (Andy Atkinson)
 // see http://github.com/webandy/jquery-tweets for a fancy jquery version
