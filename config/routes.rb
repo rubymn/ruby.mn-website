3129bf6245327d8d09301249167022eed753e7b1
Tom Brice <tomtoday@gmail.com>
2010-09-14 09:28:38 -0500

    remove stale route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.resource :archive, :member=>{:search=>:any}
   map.resources :openings
   map.resources :users, :member=>{:login=>:get}, :new=>{:validate=>:get, :forgot_password=>:get, :reset=>:post, :change_password=>:get, :set_password=>:post}, :except => [:edit, :update] do |users|
     users.resource :for_hire
