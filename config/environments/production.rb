178aaa691eeb7328c5f1996a1670a537e3b87538
Jared Mehle <jrmehle@gmail.com>
2011-11-19 12:09:01 -0600

    Heroku didn't like memcached gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ RubyMnWebsite::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
+  config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
