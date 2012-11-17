fdcdccfaa4a9c28da31c153ea92a3192bf601433
Melissa <melissavoegelimn@gmail.com>
2012-11-16 20:52:42 -0600

    removed redundant method for creating amenities_event

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -6,13 +6,4 @@ class EventRegistration < ActiveRecord::Base
   accepts_nested_attributes_for :amenities_events
   validates :email, :presence => true
 
-  attr_accessor :amenities_events_ids
-
-  def amenities_events_ids=(amenity_ids)
-    amenity_ids.each do |amenity_id|
-      a = AmenitiesEvent.where(:amenity_id => amenity_id, :event_id => event_id)
-      self.amenities_events << a
-    end
-  end
-
 end
