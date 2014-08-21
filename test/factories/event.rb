ed6a377d1dff66f542f7319215bc044d55e1cf88
Jason Hsu <jhsu@debian.debianserver.com>
2014-08-21 02:00:13 -0500

    Make only some of the seeded events approved

--- test/factories/event.rb
+++ test/factories/event.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :event do |f|
     headline { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraph }
-    approved true
+    approved { [true, false].sample }
     formatted_scheduled_time 1.day.from_now
     user
   end
