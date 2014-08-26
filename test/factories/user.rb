91cbdf09866fbc5e99e8abc70242735b38242bcf
Jason Hsu <jhsu@debian.debianserver.com>
2014-08-26 00:15:20 -0500

    Seeding now provides REAL admin (username='admin1', role='admin', password='railsrocks'); user 007 also has password 'railsrocks'; README updated

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -4,15 +4,16 @@ FactoryGirl.define do
     email { Factory.next(:email) }
     salted_password 'dcd09adae379018012f5b30e65dc5a8e8f044169'
     salt "f22c76aea1ee3faf5440369de1d4de539e4b4852"
-    password 'password'
+    password 'railsrocks'
     password_confirmation { |u|  u.password }
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
 
     factory :admin do |f|
-      login 'admin'
+      login 'admin1'
       firstname 'Admin'
       lastname 'User'
+      role 'admin'
       verified true
     end
   end
