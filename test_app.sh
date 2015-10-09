a0744cbadb50bd06e24c3adee9cb202feadf3f77
Vagrant User <vagrant@packer-debian-jessie-rbenv-rubymn.vagrantup.com>
2015-10-09 14:12:42 -0500

    Added pgadmin instructions for Vagrant setup

--- test_app.sh
+++ test_app.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 # Proper header for a Bash script.
 
-#!/bin/bash
+APP_DB_NAME_DEV='rubymn_dev'
+APP_DB_NAME_TEST='rubymn_test'
+APP_DB_USER='rubymn_user'
+APP_DB_PASS='rubymn_password'
 
 # Set up PostgreSQL
 echo '---------------------'
 echo 'Setting up PostgreSQL'
-PG_VERSION="$(ls /etc/postgresql)"
-PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
 
-# Change the settings in the pg_hba.conf file
-sudo bash -c "echo '# Database administrative login by Unix domain socket' > $PG_HBA"
-sudo bash -c "echo 'local   all             postgres                                peer' >> $PG_HBA"
-sudo bash -c "echo 'local   all             all                                     peer' >> $PG_HBA"
-sudo bash -c "echo 'host    all             all             127.0.0.1/32            md5'  >> $PG_HBA"
-sudo bash -c "echo 'host    all             all             ::1/128                 md5'  >> $PG_HBA"
-
-sudo /etc/init.d/postgresql restart # Restart PostgreSQL
-
-sudo -u postgres psql -c"CREATE ROLE $USER WITH CREATEDB LOGIN PASSWORD '';"
-sudo -u postgres psql -c"CREATE DATABASE rubymn_dev WITH OWNER=$USER;"
-sudo -u postgres psql -c"CREATE DATABASE rubymn_test WITH OWNER=$USER;"
+sudo -u postgres psql -c"CREATE ROLE $APP_DB_USER WITH CREATEDB LOGIN PASSWORD '$APP_DB_PASS';"
+sudo -u postgres psql -c"CREATE DATABASE $APP_DB_NAME_DEV WITH OWNER=$APP_DB_USER;"
+sudo -u postgres psql -c"CREATE DATABASE $APP_DB_NAME_TEST WITH OWNER=$APP_DB_USER;"
 
 echo '--------------'
 echo 'bundle install'
